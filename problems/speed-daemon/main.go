b151522f5d02f868d4e80249f6d239eac1813b6d
Nivekithan <nivekithan363@gmail.com>
2025-09-21 15:14:54 +0530

    heart beat

--- problems/speed-daemon/main.go
+++ problems/speed-daemon/main.go
@@ -1,10 +1,152 @@
 package main
 
-import "log"
+import (
+	"bufio"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"log"
+	"net"
+)
+
+func clientError(msg string) []byte {
+	clientError := ClientError{msg: msg}
+
+	return clientError.toBinary()
+
+}
+
+func hanldeConnectionImp(conn net.Conn) error {
+
+	reader := bufio.NewReader(conn)
+
+	var messageType uint8
+
+	if err := binary.Read(reader, binary.BigEndian, &messageType); err != nil {
+		return err
+	}
+
+	switch messageType {
+	case 0x80:
+		log.Println("MessageType=IamCamera")
+		camera, err := NewIamCamera(reader)
+
+		if err != nil {
+			return err
+		}
+
+		log.Printf("%+v\n", camera)
+
+		isWantHeartbeat := false
+
+		for {
+			var messageType uint8
+
+			if err := binary.Read(reader, binary.BigEndian, &messageType); err != nil {
+				return err
+			}
+
+			switch messageType {
+			case 0x40:
+				log.Println("MessageType=WantHeartbeat")
+
+				if isWantHeartbeat {
+					error := ClientError{msg: fmt.Sprintf("Multiple heartbeats not allowed: %x", messageType)}
+
+					if _, err := conn.Write(error.toBinary()); err != nil {
+						return err
+					}
+					return errors.New(error.msg)
+				}
+
+				isWantHeartbeat = true
+
+				hearbeat, err := NewWantHeartbeat(reader)
+
+				if err != nil {
+					return err
+				}
+
+				go hearbeat.SendHeartBeat(conn)
+
+			case 0x20:
+				log.Println("MessageType=Plate")
+			default:
+				error := ClientError{msg: fmt.Sprintf("unknown messageType: %x", messageType)}
+
+				if _, err := conn.Write(error.toBinary()); err != nil {
+					return err
+				}
+
+				return errors.New(error.msg)
+			}
+		}
+
+	case 0x81:
+		log.Println("MessageType=IamDispatcher")
+		camera, err := NewIamDispatcher(reader)
+
+		if err != nil {
+			return err
+		}
+		log.Printf("%+v\n", camera)
+
+		for {
+
+		}
+
+	default:
+		error := ClientError{msg: fmt.Sprintf("unknown messageType: %x", messageType)}
+
+		if _, err := conn.Write(error.toBinary()); err != nil {
+			return err
+		}
+
+		return errors.New(error.msg)
+	}
+}
+
+// This function blocks
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	defer log.Println("Closing connection")
+
+	if err := hanldeConnectionImp(conn); err != nil {
+		log.Println("error: ", err)
+	}
+}
+
+func handleListner(listner net.Listener) error {
+	conn, err := listner.Accept()
+
+	if err != nil {
+		return err
+	}
+
+	go handleConnection(conn)
+
+	return nil
+}
 
 func run() error {
 
-	return nil
+	listner, err := net.Listen("tcp", ":8000")
+
+	if err != nil {
+		return err
+	}
+
+	log.Println("Listening in port :8000")
+
+	for {
+
+		err := handleListner(listner)
+
+		if err != nil {
+			log.Println("error: handleListner", err)
+		}
+
+	}
 
 }
 
--- problems/speed-daemon/types.go
+++ problems/speed-daemon/types.go
@@ -1,7 +1,21 @@
 package main
 
+import (
+	"encoding/binary"
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
 type ClientError struct {
 	msg string
+}
+
+func (e *ClientError) toBinary() []byte {
+	output := []byte{byte(0x10)}
+	output = append(output, []byte(e.msg)...)
+	return output
 }
 
 type Plate struct {
@@ -27,7 +41,51 @@ type WantHeartbeat struct {
 	interval uint32
 }
 
+func NewWantHeartbeat(reader io.Reader) (*WantHeartbeat, error) {
+	var interval uint32
+
+	if err := binary.Read(reader, binary.BigEndian, &interval); err != nil {
+		return nil, err
+	}
+
+	return &WantHeartbeat{
+		interval: interval,
+	}, nil
+}
+
+// This function blocks
+func (heartbeat *WantHeartbeat) SendHeartBeat(conn net.Conn) {
+
+	if heartbeat.interval == 0 {
+		log.Println("heartbeat interval is zero")
+		return
+	}
+
+	interval := int(heartbeat.interval)
+
+	timer := time.NewTicker(time.Duration(interval) * (time.Second / 10))
+
+	for {
+		<-timer.C
+
+		heartbeat := &Heartbeat{}
+		if _, err := conn.Write(heartbeat.toBinary()); err != nil {
+			log.Println("error sending heartbeat:", err)
+			log.Println("Stoppping Sendheart")
+			return
+		}
+
+		log.Println("Sent hearbeat")
+	}
+
+}
+
 type Heartbeat struct {
+}
+
+func (h *Heartbeat) toBinary() []byte {
+	return []byte{0x41}
+
 }
 
 type IAmCamera struct {
@@ -36,7 +94,52 @@ type IAmCamera struct {
 	limit uint16
 }
 
+func NewIamCamera(reader io.Reader) (*IAmCamera, error) {
+	var road, mile, limit uint16
+
+	if err := binary.Read(reader, binary.BigEndian, &road); err != nil {
+		return nil, err
+	}
+
+	if err := binary.Read(reader, binary.BigEndian, &mile); err != nil {
+		return nil, err
+	}
+
+	if err := binary.Read(reader, binary.BigEndian, &limit); err != nil {
+		return nil, err
+	}
+
+	return &IAmCamera{
+		road:  road,
+		mile:  mile,
+		limit: limit,
+	}, nil
+}
+
 type IamDispatcher struct {
-	numRoads uint8
-	roads    []uint16
+	roads []uint16
+}
+
+func NewIamDispatcher(reader io.Reader) (*IamDispatcher, error) {
+	var numOfRoads uint8
+
+	if err := binary.Read(reader, binary.BigEndian, &numOfRoads); err != nil {
+		return nil, err
+	}
+
+	roads := []uint16{}
+
+	for i := 0; i < int(numOfRoads); i++ {
+		var road uint16
+
+		if err := binary.Read(reader, binary.BigEndian, &road); err != nil {
+			return nil, err
+		}
+
+		roads = append(roads, road)
+	}
+
+	return &IamDispatcher{
+		roads: roads,
+	}, nil
 }
